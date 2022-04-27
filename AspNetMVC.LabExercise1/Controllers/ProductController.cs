e928992bd6438a743e1c7e807f54f958402400e9
[gabriel.kho] <[gabriel.kho@pointwest.com.ph]>
2022-04-27 21:05:41 +0800

    first commit

---
+++ AspNetMVC.LabExercise1/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using AspNetMVC.LabExercise1.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetMVC.LabExercise1.Controllers
+{
+    public class ProductController:Controller
+    {
+        private readonly ProductService _productService;
+
+        public ProductController(ProductService productService)
+        {
+            _productService = productService;
+        }
+        
+        public IActionResult List()
+        {
+            ViewData["Products"] = _productService.GetCatalog();
+            ViewData["GetGrandTotal"] = _productService.GetGrandTotal();
+            return View();
+        }
+    }
+}
---
+++ AspNetMVC.LabExercise1/Services/ProductService.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using AspNetMVC.LabExercise1.Models;
+
+namespace AspNetMVC.LabExercise1.Services
+{
+    public class ProductService
+    {
+        private List<Product> products;
+
+        public ProductService()
+        {
+            products = new List<Product>();
+
+            products.Add(new Product()
+            {
+                Id = 1,
+                Name = "Nescafe",
+                Description = "Babangon tayo!",
+                Quantity = 10,
+                Price = 10
+            });
+
+            products.Add(new Product()
+            {
+                Id = 2,
+                Name = "Kopiko Blanca",
+                Description = "Boom! Gising!",
+                Quantity = 15,
+                Price = 8
+            });
+
+            products.Add(new Product()
+            {
+                Id = 3,
+                Name = "San Mig Coffee",
+                Description = "My sugar-free, guilt-free coffee",
+                Quantity = 20,
+                Price = 9
+            });
+
+            products.Add(new Product()
+            {
+                Id = 4,
+                Name = "Great Taste Coffee",
+                Description = "Tikman ang sarap ng tamang timpla!",
+                Quantity = 5,
+                Price = 12
+            });
+
+            products.Add(new Product()
+            {
+                Id = 5,
+                Name = "Cafe Puro",
+                Description = "The delicious taste of ground coffee",
+                Quantity = 25,
+                Price = 13
+            });
+
+        }
+        public List<Product> GetCatalog()
+        {
+            return products;
+        }
+
+        public decimal GetGrandTotal()
+        {
+            decimal total = 0;
+            foreach (var product in products)
+            {
+                total += product.TotalAmount;
+            }
+            return total;
+        }
+    }
+}
